9f5a3c944ecd60fda33fba5319317388d24cda81
Björn Merschmeier <merschibjoern@aol.de>
2018-01-29 13:37:09 +0100

    Fehlende Exceptions hinzugefügt

---
+++ PhaseTen-common/ejbModule/java/de/fh_dortmund/inf/cw/phaseten/server/exceptions/UserIsPlayerException.java
@@ -0,0 +1,10 @@
+package de.fh_dortmund.inf.cw.phaseten.server.exceptions;
+
+public class UserIsPlayerException extends Exception {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1300587517535707664L;
+
+}
---
+++ PhaseTen-common/ejbModule/java/de/fh_dortmund/inf/cw/phaseten/server/exceptions/UserIsSpectatorException.java
@@ -0,0 +1,11 @@
+package de.fh_dortmund.inf.cw.phaseten.server.exceptions;
+
+public class UserIsSpectatorException extends Exception {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = -2177015122657211442L;
+
+
+}
